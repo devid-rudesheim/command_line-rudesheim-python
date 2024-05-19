2ec57fd98e3f87eac1ea4425360cd9db16973ec5
devid-rudesheim <87573287+devid-rudesheim@users.noreply.github.com>
2024-05-19 19:32:33 +0900

    Initial commit

---
+++ lib/rudesheim/command_line/__init__.py
@@ -0,0 +1,248 @@
+import getopt as go
+
+class BasicException( Exception ):
+	pass
+
+class UndefinedOptionSpecified( BasicException ):
+	pass
+
+class OptionIsInConflict( BasicException ):
+	pass
+
+class DefaultDoesNotExist( BasicException ):
+	pass
+
+class ItemForhelp:
+
+	@classmethod
+	def description( this ):
+		return this.__name__
+
+class DefineOfOption( ItemForhelp ):
+
+	def option( this ):
+		return this.option_
+
+	def keys( this ):
+		return this.keys_
+
+	def description( this ):
+		return this.description_
+
+	def __init__( this, option, keys, description ):
+		this.option_ = option
+		this.keys_ = keys
+		this.description_ = description
+
+class Option( ItemForhelp ):
+
+	@classmethod
+	def value_amount( this ):
+		return 0
+
+	@classmethod
+	def with_values( this, strings ):
+		return this
+
+	@classmethod
+	def tie( this, keys, description ):
+		return DefineOfOption( this, keys, description )
+
+	@classmethod
+	def basic_tie( this, keys ):
+		return this.tie( keys, this.description() )
+
+
+class Mode( ItemForhelp ):
+
+	@classmethod
+	def options_defines( this ):
+		return ()
+	
+	@classmethod
+	def default( this ):
+		options = this.options_defines()
+		if 0 == len( options ):
+			raise DefaultDoesNotExist()
+
+		return options.option()
+
+class KeyDecorator:
+
+	@classmethod
+	def convert_string( this, key ):
+		return [ "-", "--" ][ 1 != len( key ) ] + key
+
+class ShortKeyDecorator:
+
+	@classmethod
+	def select( this, container ):
+		return container[0]
+
+	@classmethod
+	def convert_for_external( this, key ):
+		return "-" + key
+
+	@classmethod
+	def convert_for_spec( this, key ):
+		return key + ":"
+
+class LongKeyDecorator:
+
+	@classmethod
+	def select( this, container ):
+		return container[1]
+
+	@classmethod
+	def convert_for_external( this, key ):
+		return "--" + key
+
+	@classmethod
+	def convert_for_spec( this, key ):
+		return key + "="
+
+def key_decorator_for( key ):
+	return [ ShortKeyDecorator, LongKeyDecorator ][ 1 != len( key ) ]
+
+class Parser:
+
+	def parse( this, arguments ):
+		options = {}
+		keys_specs = ( [], [] )
+
+		# build lookup table and list for mapping
+		for mode in this.modes_templates_:
+			for define in mode.options_defines():
+				for key in define.keys():
+					option = define.option()
+					amount = 0 < option.value_amount()
+
+					decorator = key_decorator_for( key )
+
+					key_value = ( mode, option )
+					options[ decorator.convert_for_external( key ) ] = key_value
+
+					decorator.select( keys_specs ).append( [ key, decorator.convert_for_spec( key ) ][amount] )
+
+		try:
+			result = go.getopt( arguments, "".join( keys_specs[0] ), keys_specs[1] )
+
+			modes = {}
+			# for inputs
+			for key_value in result[0]:
+				define = options[ key_value[0] ]
+
+				mode = define[0]
+				if mode in modes:
+					raise OptionIsInConflict()
+
+				option = define[1]
+				if 0 < option.value_amount():
+					option = option.with_values( [ key_value[1] ] )
+
+				modes[ mode ] = option
+
+			# for default value
+			for mode in this.modes_templates_:
+
+				if mode in modes:
+					continue
+
+				modes[ mode ] = mode.default()
+
+			return ( modes, result[1] )
+
+		except go.GetoptError as exception:
+
+			if 0 < str( exception ).find( "not recognize" ):
+				raise UndefinedOptionSpecified() from exception
+
+	def __init__( this, modes_templates ):
+		this.modes_templates_ = modes_templates
+
+
+class OptionForRun( Option ):
+
+	@classmethod
+	def run_with( this, mode, arguments ):
+		pass
+
+class OptionForPrint( OptionForRun ):
+
+	@classmethod
+	def print_string( this ):
+		return ""
+
+	@classmethod
+	def run_with( this, mode, arguments ):
+		print( this.print_string() )
+
+class BasicVersion( OptionForPrint ):
+
+	@classmethod
+	def description( this ):
+		return "Print version information and exit"
+
+	@classmethod
+	def product_name( this ):
+		return ""
+
+	@classmethod
+	def numbers( this ):
+		return ( 1, 0 )
+
+	@classmethod
+	def print_string( this ):
+		product_name = ""
+
+		if 0 < len( this.product_name() ):
+			product_name = this.product_name() + ", "
+
+		return product_name + "version " + ".".join( [ str( i ) for i in this.numbers() ] )
+
+
+class BasicHelp( OptionForPrint ):
+
+	@classmethod
+	def description( this ):
+		return "Print Help (this message) and exit"
+
+	@classmethod
+	def usage( this ):
+		return []
+
+	def print_string( this ):
+		max_length = 0
+		
+		lines_elements = []
+
+		for mode in this.modes_templates:
+			line_elements = []
+
+			for define in mode.options_defines():
+				keys = ",".join( [ key_decorator_for( key ).convert_for_external( key ) for key in define.keys() ] )
+
+				max_length = max( max_length, len( keys ) )
+				line_elements.append( ( define.description(), keys ) )
+
+			lines_elements.append( ( mode.description(), line_elements ) )
+
+		lines = []
+		for i in this.usage():
+			lines.append( "usage: " + i )
+
+
+		max_length += 1
+
+		lines.append( "options:" )
+		for mode in lines_elements:
+			lines.append( "\t{0}" .format( mode[0] ) )
+
+			for define in mode[1]:
+				lines.append( ( "\t\t{0:" + str( max_length ) +"}{1}" ).format( define[1] , define[0] ) )
+
+
+		return "\n".join( lines )
+
+	def __init__( this, modes_templates ):
+		this.modes_templates = modes_templates
---
+++ test/run.py
@@ -0,0 +1,234 @@
+#!/opt/homebrew/bin/python3
+
+import rudesheim.command_line as cl
+import unittest as ut
+
+class Option_0( cl.Option ):
+	pass
+
+class Option_1( cl.Option ):
+	pass
+
+class Option_2( cl.Option ):
+	pass
+
+class Option_3( cl.Option ):
+
+	def values( this ):
+		return this.strings
+
+	def __init__( this, strings ):
+		this.strings = strings
+
+	@classmethod
+	def value_amount( this ):
+		return 1
+
+	@classmethod
+	def with_values( this, strings ):
+		return this( strings )
+
+class Mode_0( cl.Mode ):
+
+	@classmethod
+	def options_defines( this ):
+		return [ Option_1.tie( ( 'v', 'version' ), "Print version" ), Option_2.tie( ( 'h', 'help' ), "Print help" ) ]
+	
+	@classmethod
+	def default( this ):
+		return Option_0
+
+class Mode_1( cl.Mode ):
+
+	@classmethod
+	def options_defines( this ):
+		return [ Option_3.tie( ( 'd', 'depth' ), "depth" ) ]
+	
+	@classmethod
+	def default( this ):
+		return Option_0
+
+class ParserTests( ut.TestCase ):
+
+	def test_0( this ):
+		parser = cl.Parser( [] )
+
+		result = parser.parse( [] )
+
+		this.assertEqual( 2, len( result ) )
+		this.assertEqual( 0, len( result[0] ) )
+		this.assertEqual( 0, len( result[1] ) )
+
+	def test_1( this ):
+		parser = cl.Parser( [] )
+
+		arguments = [ 'one', 'two' ]
+		result = parser.parse( arguments )
+
+		this.assertEqual( 0, len( result[0] ) )
+		this.assertEqual( 2, len( result[1] ) )
+
+		this.assertEqual( arguments, result[1] )
+
+	def test_2( this ):
+		parser = cl.Parser( [] )
+
+		with this.assertRaises( cl.UndefinedOptionSpecified ):
+			parser.parse( [ "-h" ] )
+
+
+	def test_3( this ):
+		parser = cl.Parser( [ Mode_0 ] )
+
+		with this.assertRaises( cl.OptionIsInConflict ):
+			parser.parse( [ "-v", "-v" ] )
+
+	def test_4( this ):
+		parser = cl.Parser( [ Mode_0 ] )
+
+		arguments = [ 'one' ]
+		result = parser.parse( arguments )
+
+		this.assertEqual( 1, len( result[0] ) )
+		this.assertEqual( 1, len( result[1] ) )
+
+		this.assertEqual( arguments, result[1] )
+
+	def test_5( this ):
+		parser = cl.Parser( [ Mode_0 ] )
+
+		result = parser.parse( [] )
+
+		this.assertEqual( 1, len( result[0] ) )
+		this.assertEqual( 0, len( result[1] ) )
+
+		this.assertEqual( { Mode_0 : Option_0 }, result[0] )
+
+	def test_6( this ):
+		parser = cl.Parser( [ Mode_0 ] )
+
+		result = parser.parse( [ "-v" ] )
+
+		this.assertEqual( 1, len( result[0] ) )
+		this.assertEqual( 0, len( result[1] ) )
+
+		this.assertEqual( { Mode_0 : Option_1 }, result[0] )
+
+	def test_7( this ):
+		parser = cl.Parser( [ Mode_0 ] )
+
+		result = parser.parse( [ "--version" ] )
+
+		this.assertEqual( 1, len( result[0] ) )
+		this.assertEqual( 0, len( result[1] ) )
+
+		this.assertEqual( { Mode_0 : Option_1 }, result[0] )
+
+	def test_8( this ):
+		parser = cl.Parser( [ Mode_0 ] )
+		argument = "value"
+
+		result = parser.parse( [ "--version", argument ] )
+
+		this.assertEqual( 1, len( result[0] ) )
+		this.assertEqual( 1, len( result[1] ) )
+
+		this.assertEqual( { Mode_0 : Option_1 }, result[0] )
+		this.assertEqual( argument, result[1][0] )
+
+	def test_9( this ):
+		parser = cl.Parser( [ Mode_0 ] )
+
+		result = parser.parse( [ "-h" ] )
+
+		this.assertEqual( { Mode_0 : Option_2 }, result[0] )
+
+	def test_10( this ):
+		parser = cl.Parser( [ Mode_1 ] )
+
+		argument = "value"
+		result = parser.parse( [ "-d", argument ] )
+
+		this.assertEqual( [ argument ], result[0][Mode_1].values() )
+
+	def test_11( this ):
+		parser = cl.Parser( [ Mode_0, Mode_1 ] )
+
+		argument = "value"
+		result = parser.parse( [ "--help", "--depth", argument ] )
+
+		this.assertEqual( Option_2, result[0][Mode_0] )
+		this.assertEqual( [ argument ], result[0][Mode_1].values() )
+
+	def test_12( this ):
+		parser = cl.Parser( [ Mode_1, Mode_0 ] )
+
+		argument = "value"
+		result = parser.parse( [ "--help", "--depth", argument ] )
+
+		this.assertEqual( Option_2, result[0][Mode_0] )
+		this.assertEqual( [ argument ], result[0][Mode_1].values() )
+
+class Version_0( cl.BasicVersion ):
+
+	@classmethod
+	def product_name( this ):
+		return "Example"
+
+	@classmethod
+	def numbers( this ):
+		return ( 2, 1, 0 )
+
+class VersionTests( ut.TestCase ):
+
+	def product_name( this ):
+		return "Print version information and exit"
+
+	def test_0( this ):
+		this.assertEqual( this.product_name(), Version_0.description() )
+		this.assertEqual( this.product_name(), Version_0.basic_tie( [] ).description() )
+
+	def test_1( this ):
+		this.assertEqual( "Example, version 2.1.0", Version_0.print_string() )
+
+	def test_2( this ):
+		this.assertEqual( "version 1.0", cl.BasicVersion.print_string() )
+
+class Help_0( cl.BasicHelp ):
+
+	@classmethod
+	def usage( this ):
+		return [ "example" ]
+
+	def __init__( this, modes_templates ):
+		super( Help_0, this ).__init__( modes_templates )
+
+
+class HelpTests( ut.TestCase ):
+
+	def product_name( this ):
+		return "Print Help (this message) and exit"
+
+	def test_0( this ):
+		this.assertEqual( this.product_name(), Help_0.description() )
+		this.assertEqual( this.product_name(), Help_0.basic_tie( [] ).description() )
+
+	def test_1( this ):
+		this.assertEqual \
+		(
+		 	"\n".join \
+			(
+			 	(
+					"usage: example",
+					"options:",
+					"	Mode_0",
+					"		-v,--version Print version",
+					"		-h,--help    Print help",
+					"	Mode_1",
+					"		-d,--depth   depth"
+				)
+			),
+			Help_0( [ Mode_0, Mode_1 ] ).print_string()
+		)
+
+ut.main()
